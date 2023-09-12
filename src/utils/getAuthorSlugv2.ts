6e34644b2074c977e953834997cd5f91e12cface
mohithworks <mohithworks2002@gmail.com>
2023-09-13 00:28:24 +0530

    Fixed Bug 2

--- src/utils/getAuthorSlugv2.ts
+++ src/utils/getAuthorSlugv2.ts
@@ -12,7 +12,10 @@ const getAuthorSlug = () => {
     // console.log('location1', location)
     // console.log('location2', location2)
     // console.log('url', url)
-
+    if(hostname === "main.d2engxoisy6yba.amplifyapp.com") {
+        const authorSlug = { custom_domain: false, domain1: 'jennifers-personal-blog', domain2: 'no' };
+        return authorSlug;
+    }
     const authorSlug:any = location != url ? location2 !== process.env.NEXT_SITE_NAME ? { custom_domain: true, domain1: location, domain2: location2 } : { custom_domain: false, domain1: location, domain2: 'no' } : { custom_domain: false, domain1: 'hrithik', domain2: 'no' };
 
     //console.log('authorSlug', authorSlug)
